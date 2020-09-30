a10ff29dd345c1367eeead4815ed41b45e8b41e0
HezkielRivaldoSiregar <hezkiel.rivaldo@ti.ukdw.ac.id>
2020-09-30 22:44:22 +0700

    -simple changes

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: dipanggil");
         setContentView(R.layout.activity_main);
     }
 }
